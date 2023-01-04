64abb16bc21385c8ac0a0ab003e5b104c038e766
dan-medina <dmedinamail@gmail.com>
2023-01-03 18:28:48 -0800

    created read_transaction to process each transaction and account for multiple transactions in one day

--- account_transactions.py
+++ account_transactions.py
@@ -55,39 +55,53 @@ transaction_data = {}
 #     return 
 
 def read_transactions():
-    with open(input_file, newlien='') as csvfile:
+    with open(input_file, newline='') as csvfile:
         reader = csv.reader(csvfile)
         next(reader)
         for row in reader:
             #checking if line is empty
             if not row[0]:
                 continue
-        parsed_row = row[1].split("/")
-        month_key = parsed_row[0] + "/" + parsed_row[2]
-        day = parsed_row[1]
+            parsed_row = row[1].split("/")
+            month_key = parsed_row[0] + "/" + parsed_row[2]
+            day = parsed_row[1]
 
-        #checking if customer id has been seen before
-        if row[0] not in transaction_data:
-            #if not create new dictionary to store new customer's monthly transactions
-            transaction_data[row[0]] = {}
-        #check to see if there are any transactions for current month/year
-        if month_key not in transaction_data[row[0]]:
-            #if not begin tracking transaction data for current month/year
-            transaction_data[row[0]][month_key] = {}
-        #check to see if there are any transactions for current day
-        if day not in transaction_data[row[0]][month_key]:
-            #if not begin tracking transaction data for current day 
-            transaction_data[row[0]][month_key][day] = []
-        #add current transaction
-        customer_data[row[0]][month_key][day].append(int(row[2]))
+            #checking if customer id has been seen before
+            if row[0] not in transaction_data:
+                #if not create new dictionary to store new customer's monthly transactions
+                transaction_data[row[0]] = {}
+            #check to see if there are any transactions for current month/year
+            if month_key not in transaction_data[row[0]]:
+                #if not begin tracking transaction data for current month/year
+                transaction_data[row[0]][month_key] = {}
+            #check to see if there are any transactions for current day
+            if day not in transaction_data[row[0]][month_key]:
+                #if not begin tracking transaction data for current day 
+                transaction_data[row[0]][month_key][day] = []
+            #add current transaction
+            transaction_data[row[0]][month_key][day].append(int(row[2]))
+
 
     return
 
 def store_customer_data():
+
+
+
     return
 
 def create_output():
+    with open(output_file, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        #iterate through each stored customer id
+        for customer in transaction_data:
+            #iterate through customer's data for each month
+            for month in transaction_data[customer]:
+                #write data for current month to output csv file
+                writer.writerow([customer, str(month), transaction_data[customer][month]])
     return
 
 
 read_transactions()
+create_output()
