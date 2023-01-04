a2acae46194c08c1bdc4012a712c404a00eb221d
dan-medina <dmedinamail@gmail.com>
2023-01-03 20:10:54 -0800

    added comments to new functions

--- account_transactions.py
+++ account_transactions.py
@@ -8,51 +8,11 @@ if len(sys.argv) < 2:
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
+#dictionary to store relevant monthly customer info including MinBalance, MaxBalance, and EndingBalance
 customer_data = {}
 
+#dictionary to store transactions for a given month for a given customer
 transaction_data = {}
-
-# def read_transactions():
-    
-
-#     #read from input csv file
-#     with open(input_file, newline='') as csvfile:
-#         reader = csv.reader(csvfile)
-#         next(reader)
-#         for row in reader:
-#             #checking if line is empty
-#             if not row[0]:
-#                 continue
-
-#             parsed_row = row[1].split("/")
-#             month_key = parsed_row[0] + "/" + parsed_row[2]
-
-#             #checking if customer id has been seen before
-#             if row[0] not in customer_data:
-#                 # if not create new dictionary to store new customer's monthly transaction information
-#                 customer_data[row[0]] = {}
-#             #if customer has made transactions, check to see if any transactions for current month/year
-#             if month_key not in customer_data[row[0]]:
-#                 #if not begin tracking transaction data for current month/year
-#                 customer_data[row[0]][month_key] = {"MinBalance": int(row[2]), "MaxBalance": int(row[2]), "Ending Balance": 0, "Transactions":{}}
-
-#             #updating customer's monthly data based on current transaction
-#             customer_data[row[0]][month_key]["Ending Balance"] += int(row[2])
-#             customer_data[row[0]][month_key]["MinBalance"] = min(customer_data[row[0]][month_key]["MinBalance"], customer_data[row[0]][month_key]["Ending Balance"])
-#             customer_data[row[0]][month_key]["MaxBalance"] = max(customer_data[row[0]][month_key]["MaxBalance"], customer_data[row[0]][month_key]["Ending Balance"])
-    
-#     #write to output csv file
-#     with open(output_file, 'w', newline='') as csvfile:
-#         writer = csv.writer(csvfile)
-
-#         #iterate through each stored customer id
-#         for customer in customer_data:
-#             #iterate through customer's data for each month
-#             for month in customer_data[customer]:
-#                 #write data for current month to output csv file
-#                 writer.writerow([customer, str(month), customer_data[customer][month]["MinBalance"], customer_data[customer][month]["MaxBalance"], customer_data[customer][month]["Ending Balance"]])
-
-#     return 
 
 def read_transactions():
     with open(input_file, newline='') as csvfile:
@@ -92,16 +52,23 @@ def store_customer_data():
             min_balance = 0
             max_balance = 0
             for i, day in enumerate(transaction_data[customer][month]):
+
+                #ensures that for days with multiple transactions, credits are applied first, before debits
                 trans_sorted = sorted(transaction_data[customer][month][day], reverse=True)
                 if i == 0:
                     curr_balance += int(trans_sorted[0])
+
+                    #min and max balance variables begin tracking after first transaction 
                     min_balance = int(trans_sorted[0])
                     max_balance = int(trans_sorted[0])
+
+                    #process any remaining transactions for the first day 
                     for j in range(1, len(trans_sorted)):
                         curr_balance += int(trans_sorted[j])
                         min_balance = min(min_balance, curr_balance)
                         max_balance = max(max_balance, curr_balance)
                 else:
+                    #process all other days for current month/year
                     for j in range(0, len(trans_sorted)):
                         curr_balance += int(trans_sorted[j])
                         min_balance = min(min_balance, curr_balance)
